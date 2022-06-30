07c5fa663c848402ec55fddb2d3b29fe99bd212e
Stacy <TianXianHu@users.noreply.github.com>
2022-07-01 00:30:51 +0300

    refactored

--- src/index.js
+++ src/index.js
@@ -1,41 +1,21 @@
-function cToF(celsius) {
-  var cTemp = celsius;
-  return (cTemp * 9) / 5 + 32;
+var metricTemp = null;
+
+function dateConvert(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return days[date.getDay()] + " " + date.getHours() + ":" + date.getMinutes();
 }
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let now = new Date();
-
-let today = document.querySelector("#todate");
-today.innerHTML =
-  days[now.getDay()] + " " + now.getHours() + ":" + now.getMinutes();
-
-/*
-function chHeader(event) {
-  event.preventDefault();
-  var header = document.querySelector("h1");
-  var search = document.querySelector("#searchinput");
-  if (search.value) {
-    header.innerHTML = search.value;
-  }
-}
-
-document.querySelector("#C").addEventListener("click", tswitchC);
-document.querySelector("#F").addEventListener("click", tswitchF); */
 
 function handlePosition(position) {
   var lon = position.coords.latitude;
-  console.log(lon);
   var lat = position.coords.longitude;
-  console.log(lat);
   const key = "2b6fdad0cbd018949c50c70f72250726";
   var url =
     "https://api.openweathermap.org/data/2.5/forecast?lat=" +
@@ -43,15 +23,24 @@ function handlePosition(position) {
     "&lon=" +
     lon +
     "&appid=" +
-    key;
+    key +
+    "&units=metric";
   fetch(url)
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      var temp = Math.round(data.list[0].main.temp - 273.15);
-      document.querySelector("h1").innerHTML =
-        "It is " + temp + " degrees in current place";
+      var temp = Math.round(data.list[0].main.temp);
+      document.querySelector("h1").innerHTML = "Current place";
+      document.querySelector(".temperature").innerHTML = temp;
+      document.querySelector("#todate").innerHTML = dateConvert(
+        (dt = new Date(data.list[0].dt * 1000))
+      );
+      document.querySelector(".wording").innerHTML =
+        data.list[0].weather[0].description;
+      document.querySelector(".press").innerHTML = data.list[0].main.pressure;
+      document.querySelector(".hum").innerHTML = data.list[0].main.humidity;
+      document.querySelector(".wind").innerHTML = data.list[0].wind.speed;
     });
 }
 
@@ -73,15 +62,42 @@ function showData(event) {
       .then((data) => {
         console.log(data);
         var temp = data.list[0].main.temp;
-        document.querySelector("h1").innerHTML =
-          "It is " + temp + " degrees in " + data.city.name;
-        document.querySelector(".temperature").innerHTML = temp;
+        document.querySelector("h1").innerHTML = data.city.name;
+        document.querySelector(".temperature").innerHTML = Math.round(temp);
+        document.querySelector("#todate").innerHTML = dateConvert(
+          (dt = new Date(data.list[0].dt * 1000))
+        );
+        document.querySelector(".wording").innerHTML =
+          data.list[0].weather[0].description;
+        document.querySelector(".press").innerHTML = data.list[0].main.pressure;
+        document.querySelector(".hum").innerHTML = data.list[0].main.humidity;
+        document.querySelector(".wind").innerHTML = data.list[0].wind.speed;
       });
   }
 }
 
-let crrntbtn = document.querySelector("#crrntbtn");
-crrntbtn.addEventListener("click", showCurrentTemp);
+function tswitchC(event) {
+  event.preventDefault();
+  if (!metricTemp) {
+    metricTemp = document.querySelector(".temperature").innerText;
+  }
+  document.querySelector("#C").classList.add("lightoff");
+  document.querySelector("#F").classList.remove("lightoff");
+  document.querySelector(".temperature").innerHTML = metricTemp;
+}
+function tswitchF(event) {
+  event.preventDefault();
+  if (!metricTemp) {
+    metricTemp = document.querySelector(".temperature").innerText;
+  }
+  document.querySelector("#C").classList.remove("lightoff");
+  document.querySelector("#F").classList.add("lightoff");
+  document.querySelector(".temperature").innerHTML = Math.round(
+    (metricTemp * 5) / 9 + 32
+  );
+}
 
-let form = document.querySelector(".search-form");
-form.addEventListener("submit", showData);
+document.querySelector("#crrntbtn").addEventListener("click", showCurrentTemp);
+document.querySelector(".search-form").addEventListener("submit", showData);
+document.querySelector("#C").addEventListener("click", tswitchC);
+document.querySelector("#F").addEventListener("click", tswitchF);
